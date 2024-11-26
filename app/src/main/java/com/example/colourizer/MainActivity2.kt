acffe30c24f42f4ce6212eb0620062aa891f7f82
Pavithra <pavithraganta0924@gmail.com>
2024-11-26 18:27:27 +0530

    Uploaded the first half on Github

---
+++ app/src/main/java/com/example/colourizer/MainActivity2.kt
@@ -0,0 +1,42 @@
+package com.example.colourizer
+
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.github.dhaval2404.imagepicker.ImagePicker
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var img: ImageView
+    private lateinit var btn: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        img=findViewById(R.id.imageView3)
+        btn=findViewById(R.id.button4)
+        btn.setOnClickListener{
+            ImagePicker.with(this)
+                .crop()	    			//Crop image(Optional), Check Customization for more option
+                .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                .start()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        img.setImageURI(data?.data)
+    }
+}
